3091b1efb7ade1d9ab7b881eeb3ee2f8a3567244
ersutton <esutton@pakton.com.au>
2018-08-08 11:40:32 +1000

    [ci skip] updating to have it as it used to be

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as rp from 'request-promise';
 export interface IGetAuthTokenResult {
   accesstoken: string;
   expirytime: number;
@@ -10,38 +11,85 @@ export interface ITokenObj {
   refresh_token: string;
 }
 
-let iGetAuthTokenResult: IGetAuthTokenResult;
-
-export function saveOAuthTokens(token_obj: ITokenObj): Promise<void> {
-  return new Promise(function(resolve, reject) {
-    //iGetAuthTokenResult = accessTokenHack(token_obj);
-    iGetAuthTokenResult = {
-      accesstoken: token_obj.access_token,
-      expirytime: token_obj.expiry_time,
-      refreshtoken: token_obj.refresh_token,
-    };
-    resolve();
-  });
+export interface IStorage {
+  saveOAuthTokens: (token_obj: ITokenObj) => Promise<void>;
+  updateOAuthTokens: (token_obj: ITokenObj) => Promise<void>;
+  getOAuthTokens(user_identifier: string): Promise<IGetAuthTokenResult[]>;
 }
 
-export function updateOAuthTokens(token_obj: ITokenObj): Promise<void> {
-  return new Promise(function(resolve, reject) {
-    //iGetAuthTokenResult = accessTokenHack(token_obj);
-    iGetAuthTokenResult = {
-      accesstoken: token_obj.access_token,
-      expirytime: token_obj.expiry_time,
-      refreshtoken: token_obj.refresh_token,
-    };
-    resolve();
-  });
-}
-
-export function getOAuthTokens(
-  user_identifier
-): Promise<IGetAuthTokenResult[]> {
-  return new Promise(function(resolve, reject) {
-    const result: IGetAuthTokenResult = { ...iGetAuthTokenResult };
-    var result_array: IGetAuthTokenResult[] = [result];
-    resolve(result_array);
-  });
+const oauthKey = 'oauthKey';
+export function init(
+  context,
+  account: string,
+  token: string,
+  environment: string
+): IStorage {
+  const _context = context;
+  function postStore(key: string, value: string): Promise<IStorage> {
+    return rp({
+      uri:
+        'https://api.spotinst.io/functions/environment/' +
+        environment +
+        '/userDocument',
+      method: 'POST',
+      qs: { accountId: account },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      body: {
+        userDocument: {
+          key: key,
+          value: value,
+        },
+      },
+      json: true,
+    });
+  }
+  const returnObj: IStorage = {
+    saveOAuthTokens: function saveOAuthTokens(
+      token_obj: ITokenObj
+    ): Promise<void> {
+      return new Promise(function(resolve, reject) {
+        const iGetAuthTokenResult = {
+          accesstoken: token_obj.access_token,
+          expirytime: token_obj.expiry_time,
+          refreshtoken: token_obj.refresh_token,
+        };
+        postStore(oauthKey, JSON.stringify(iGetAuthTokenResult)).then(() => {
+          resolve();
+        });
+      });
+    },
+    updateOAuthTokens: function updateOAuthTokens(
+      token_obj: ITokenObj
+    ): Promise<void> {
+      return new Promise(function(resolve, reject) {
+        const iGetAuthTokenResult = {
+          accesstoken: token_obj.access_token,
+          expirytime: token_obj.expiry_time,
+          refreshtoken: token_obj.refresh_token,
+        };
+        postStore(oauthKey, JSON.stringify(iGetAuthTokenResult)).then(() => {
+          resolve();
+        });
+      });
+    },
+    getOAuthTokens: function getOAuthTokens(
+      user_identifier: string
+    ): Promise<IGetAuthTokenResult[]> {
+      return new Promise(function(resolve, reject) {
+        _context.getDoc(oauthKey, function(err, res) {
+          if (res) {
+            const result: IGetAuthTokenResult = JSON.parse(res);
+            var result_array: IGetAuthTokenResult[] = [result];
+            resolve(result_array);
+          }
+          reject(err);
+        });
+      });
+    },
+  };
+  console.log(`about to print init: ${JSON.stringify(returnObj)}`);
+  return returnObj;
 }
